c367f40bee67d840d342bb820d65c979bcaf5af4
sjoedwards <samuel.j.o.edwards@gmail.com>
2020-12-17 17:11:39 +0000

    Square screen

---
+++ src/screens/SquareScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const SquareScreen = () => {
+  return <View />;
+};
+
+const style = StyleSheet.create({});
+
+export default SquareScreen;
